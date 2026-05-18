46279ea52277821466092da7e2ded1d81868e681
Laia123456 <marinrosaslaia@fpllefia.com>
2026-05-18 12:15:13 +0000

    js_index

---
+++ index.js
@@ -0,0 +1,6 @@
+
+
+const supaBase_url = "https://adpmdvshoumbuytbevni.supabase.co/rest/v1/";
+const supabaseKey = "publishable_K5KZdf-EfO67lBsK9-_NPg_uyqiZmkz";
+const supabaseGeneral = supabaseKey.createClient(supaBase_url, supabaseKey);
+
