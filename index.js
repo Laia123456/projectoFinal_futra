b80621490ae74f4dd3d749aa83c26f03310d962a
Laia123456 <marinrosaslaia@fpllefia.com>
2026-05-28 17:45:24 +0000

    alineacions_comentadoOK

--- index.js
+++
@@ -1,6 +0,0 @@
-
-
-const supaBase_url = "https://adpmdvshoumbuytbevni.supabase.co/rest/v1/";
-const supabaseKey = "publishable_K5KZdf-EfO67lBsK9-_NPg_uyqiZmkz";
-const supabaseGeneral = supabaseKey.createClient(supaBase_url, supabaseKey);
-
